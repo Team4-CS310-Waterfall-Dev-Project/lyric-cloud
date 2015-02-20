524ca630ab7cffbb7969964989e64c656ad3ba77
anthonywittemann <awittema@usc.edu>
2015-02-19 18:27:23 -0800

    used yeoman to create scaffold

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lyricCloudApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the lyricCloudApp
+ */
+angular.module('lyricCloudApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
