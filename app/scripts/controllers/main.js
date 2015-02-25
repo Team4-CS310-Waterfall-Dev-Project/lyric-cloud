f04cedd061adc236588072e814e08dcb279f9ce4
anthonywittemann <awittema@usc.edu>
2015-02-25 12:09:28 -0800

    passing artists between controllers

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,15 +8,38 @@
  * Controller of the lyricCloudApp
  */
 angular.module('lyricCloudApp')
-    .controller('MainCtrl', function($scope, $location) {
-        $scope.awesomeThings = [
-            'HTML5 Boilerplate',
-            'AngularJS',
-            'Karma'
-        ];
+    .controller('MainCtrl', function($scope, $location, sharedProperties) {
+        $scope.artists = [];
 
         $scope.displayWordCloud = function() {
             $location.path('/word-cloud/');
             //TODO call php to display word cloud
         };
-    });
+
+        //called when the user presses submit
+        $scope.newArtist = function(artistName) {
+            $scope.artists = [artistName];
+
+            //TODO call php to display word cloud
+
+            console.log($scope.artists);
+            $scope.displayWordCloud();
+            sharedProperties.setProperty($scope.artists)
+            //TODO pass the artist currently being used to word-cloud
+        };
+    })
+
+.service('sharedProperties', function() {
+    var property = {
+        artists: 'First Artist'
+    };
+
+    return {
+        getProperty: function() {
+            return property;
+        },
+        setProperty: function(value) {
+            property = value;
+        }
+    };
+});
