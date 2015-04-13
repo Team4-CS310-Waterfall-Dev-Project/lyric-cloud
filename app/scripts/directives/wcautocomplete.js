3fc540b0859353fdd53d1004bc13505eadc2443b
anthonywittemann <awittema@usc.edu>
2015-04-12 23:18:03 -0700

    fixed a semicolon, now it's working

--- app/scripts/directives/wcautocomplete.js
+++ app/scripts/directives/wcautocomplete.js
@@ -8,44 +8,45 @@
  */
 angular.module('lyricCloudApp')
     .directive('wcAutocomplete', function() {
-            return {
-                template: '<div></div>',
-                restrict: 'EACM',
-                link: function postLink(scope, element, attrs) {
-                    $(function() {
-                        //TODO change to actual files
-                        var availableTags = [
-                            'ActionScript',
-                            'AppleScript',
-                            'Asp',
-                            'BASIC',
-                            'C',
-                            'C++',
-                            'Clojure',
-                            'COBOL',
-                            'ColdFusion',
-                            'Erlang',
-                            'Fortran',
-                            'Groovy',
-                            'Haskell',
-                            'Java',
-                            'JavaScript',
-                            'Lisp',
-                            'Perl',
-                            'PHP',
-                            'Python',
-                            'Ruby',
-                            'Scala',
-                            'Scheme'
-                        ];
+        return {
+            template: '<div></div>',
+            restrict: 'EACM',
+            link: function postLink(scope, element, attrs) {
+                $(function() {
+                    //TODO change to actual files
+                    var availableTags = [
+                        'ActionScript',
+                        'AppleScript',
+                        'Asp',
+                        'BASIC',
+                        'C',
+                        'C++',
+                        'Clojure',
+                        'COBOL',
+                        'ColdFusion',
+                        'Erlang',
+                        'Fortran',
+                        'Groovy',
+                        'Haskell',
+                        'Java',
+                        'JavaScript',
+                        'Lisp',
+                        'Perl',
+                        'PHP',
+                        'Python',
+                        'Ruby',
+                        'Scala',
+                        'Scheme'
+                    ];
 
-                        $('#topic_title').autocomplete({
-                            source: availableTags
+                    $('#topic_title').autocomplete({
+                        source: availableTags
 
-                            //TODO change the source
-                            //source: '/path/to/ajax_autocomplete.php',
-                        });
-
+                        //TODO change the source
+                        //source: '/path/to/ajax_autocomplete.php',
                     });
-                };
-            });
+
+                });
+            }
+        };
+    });
