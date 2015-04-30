e57060e78cd55ecda662ac37bcc0d9d90cf0a9dd
anthonywittemann <awittema@usc.edu>
2015-04-29 19:51:48 -0700

    tests for implementation 4

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -2,21 +2,21 @@
 
 describe('Controller: MainCtrl', function () {
 
-  // load the controller's module
-  beforeEach(module('lyricCloudApp'));
+    // load the controller's module
+    beforeEach(module('lyricCloudApp'));
 
-  var MainCtrl,
-    scope;
+    var MainCtrl,
+        scope;
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
-      $scope: scope
+    // Initialize the controller and a mock scope
+    beforeEach(inject(function ($controller, $rootScope) {
+        scope = $rootScope.$new();
+        MainCtrl = $controller('MainCtrl', {
+            $scope: scope
+        });
+    }));
+
+    it('should have an empty autocomplete box', function () {
+        expect(scope.artists.getText()).toBe("");
     });
-  }));
-
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
-  });
 });
--- test/spec/controllers/word-cloud.js
+++ test/spec/controllers/word-cloud.js
@@ -2,21 +2,21 @@
 
 describe('Controller: WordCloudCtrl', function () {
 
-  // load the controller's module
-  beforeEach(module('lyricCloudApp'));
+    // load the controller's module
+    beforeEach(module('lyricCloudApp'));
 
-  var WordCloudCtrl,
-    scope;
+    var WordCloudCtrl,
+        scope;
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    WordCloudCtrl = $controller('WordCloudCtrl', {
-      $scope: scope
+    // Initialize the controller and a mock scope
+    beforeEach(inject(function ($controller, $rootScope) {
+        scope = $rootScope.$new();
+        WordCloudCtrl = $controller('WordCloudCtrl', {
+            $scope: scope
+        });
+    }));
+
+    it('should have a table of top 10 papers', function () {
+        expect(scope.papers.length).toBe(10);
     });
-  }));
-
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
-  });
 });
